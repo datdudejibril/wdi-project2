1def15896a142651d21619f0ef11f40ba8fdbb95
datdudejibril <datdudejibril@gmail.com>
2017-03-26 19:28:07 -0400

    scratched boilerplate and used authclass solution as started code

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,41 +1,45 @@
-var bcrypt = require('bcrypt');
 var User = require('../models/user.js');
+var bcrypt = require('bcrypt');
 
 function createSecure(req, res, next) {
-  var password = req.body.password;
+  var password = req.body.password
 
   res.hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  next();
+  next()
 }
 
 function loginUser(req, res, next) {
-  // YOU MIGHT CHANGE EMAIL TO USERNAME IF YOU DON'T WANT TO STORE EMAILS
   var email = req.body.email;
   var password = req.body.password;
 
-  var query = User.findOne({ email: email }).exec();
-
-  query.then(function(foundUser){
+  User.findOne({ email: email })
+  .then(function(foundUser){
     if (foundUser == null) {
-      res.json({status: 401, data: "unauthorized"});
+      res.json({status: 401, data: "unauthorized"})
 
     } else if (bcrypt.compareSync(password, foundUser.password_digest)) {
       req.session.currentUser = foundUser;
     }
-    next();
+    next()
   })
   .catch(function(err){
-    res.json({status: 500, data: err});
+    res.json({status: 500, data: err})
   });
 }
 
 function authorize(req, res, next) {
-  var currentUser = req.session.currentUser;
+  var currentUser = req.session.currentUser
 
+  // THIS ASSUMES THAT EVERY :id refers to the user _id
+  // needs to check if the current user doesn't exist, if it does then make
+  // sure that the id of the logged in user and the id of the route match
   if (!currentUser || currentUser._id !== req.params.id ) {
-    res.json({status: 401, data: 'unauthorized'});
+    // customize
+    // res.render('errors/401.hbs')
+    // res.redirect('/users')
+    res.send({status: 401})
   } else {
-    next();
+    next()
   }
 };
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+pry = require('pryjs');
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
@@ -5,19 +6,20 @@ var methodOverride = require('method-override');
 var logger = require('morgan');
 var hbs = require('hbs')
 var mongoose = require('mongoose');
-
+var authHelpers = require('./helpers/auth.js')
 var usersController = require('./controllers/users.js');
 var sessionsController = require('./controllers/sessions.js');
 
 var app = express();
 
-// ADD THE NAME OF YOUR DATABASE
-// mongoose.connect('mongodb://localhost/<YourDatabaseNameHere>');
+mongoose.connect('mongodb://localhost/auth');
 
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.use(logger('dev'));
 app.use(methodOverride('_method'));
 
@@ -30,4 +32,15 @@ app.use(session({
 app.use('/users', usersController);
 app.use('/sessions', sessionsController);
 
-app.listen(4000);
+function hello(req, res, next) {
+  console.log('HELLO FROM MIDDLEWARE>>>>>>>>>>>');
+  next()
+}
+
+app.get('/test-middleware', authHelpers.authorize, function(req, res) {
+  res.send('hi')
+})
+
+app.listen(3000, function() {
+  console.log('hey')
+});
