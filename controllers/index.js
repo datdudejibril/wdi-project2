a275e7257123d2ef7d847f7cb440ad811134080c
datdudejibril <datdudejibril@gmail.com>
2017-03-28 16:05:31 -0400

    index page displaying user data

--- controllers/index.js
+++ controllers/index.js
@@ -2,24 +2,17 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user.js');
 
-
 /* GET home page. */
-router.get('/', function homeAction(req, res, next) {
-
-  const allUsers = User.find({})
-   // .exec(function(err, users){
-   //      if (err) { console.log(err); }
-    const allProjects = []
-
-  allUsers.each(function (user) {
-    user.projects.each(function (project){
-      projects.push(allProjects)
-    });
+router.get('/', function(req, res) {
+  console.log(req.session)
+  User.find({})
+  .exec(function(err, users){
+    if (err) { console.log(err); }
+    res.render('users/index.hbs', {
+      users: users,
+      currentUser: req.session.currentUser
+    })
   });
-
-  res.render('home', {
-    projectsToLoopOver: allProjects
-  })
-});
+})
 
 module.exports = router;
