8d6b37510cea955c079cadf6843c4960d5f8f67d
datdudejibril <datdudejibril@gmail.com>
2017-03-29 10:43:51 -0400

    connecting to mongolab db

--- server.js
+++ server.js
@@ -14,7 +14,22 @@ var projectsController = require('./controllers/projects.js');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/rankthatproject');
+// mongoose.connect('mongodb://localhost/rankthatproject');
+// Connect to database
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/express-movies');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 app.set('view engine', 'hbs')
 
